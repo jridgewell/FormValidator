e70126c60b5cfa6f355f9ca53a257cb3c1a31a90
Justin Ridgewell <justin@ridgewell.name>
2013-05-23 02:45:53 -0400

    Major refactor for Validations.
    This is the first step to matching Rails' ActiveRecord Validations.
    
    Validations will now be functions instead of const ints, and will return
    a validation function. Form->validate will then use the returned
    functions (stored in the Form->validations array) to validate the needed
    data.

--- lib/FormValidator/Validation.php
+++ lib/FormValidator/Validation.php
@@ -6,18 +6,94 @@ class Validation {
     /**
      * The validation rules that can be used
      */
-    const CUSTOM           = -1;
-    const DO_NOTHING       = -2;
-    const EMAIL            = -3;
-    const IN_DATA_LIST     = -4;
-    const LENGTH           = -5;
-    const MUST_MATCH_FIELD = -6;
-    const MUST_MATCH_REGEX = -7;
-    const NOT_EMPTY        = -8;
-    const NUMBER           = -9;
-    const STRING           = -10;
-    const TIMEZONE         = -11;
-    const UPLOAD           = -12;
-    const URL              = -13;
+
+    public static function anything() {
+        return function() {
+            return true;
+        };
+    }
+
+    public static function presence() {
+        return function($val) {
+            return strlen($val) > 0;
+        };
+    }
+
+    public static function length($options) {
+        $max = (self::option('max', $options)) ?: PHP_INT_MAX;
+        $min = (self::option('min', $options)) ?: 0;
+        return function($val) use ($max, $min) {
+            $len = strlen($val);
+            return ($min <= $len && $len <= $max);
+        };
+    }
+
+    public static function numericality($options) {
+        $filter = (self::option('only_integer')) ? FILTER_VALIDATE_INT : FILTER_VALIDATE_FLOAT;
+        $filter_function = function($val) use ($filter) {
+            return (filter_var($val, $filter) !== false);
+        };
+
+        if (self::option('allow_nil', $options)) {
+            return $filter_function;
+        }
+
+        $checks = Array();
+        if (self::option('odd', $options)) {
+            $checks[] = function($val) {
+                return ($val % 2 === 1);
+            };
+        }
+        if (self::option('even', $options)) {
+            $checks[] = function($val) {
+                return ($val % 2 === 0);
+            };
+        }
+
+        return function($val) use ($filter_function, $checks) {
+            foreach ($checks as &$check) {
+                if ($check($val) === false) {
+                    return false;
+                }
+            }
+            return $filter_function($val);
+        };
+    }
+
+    public static function url() {
+        return function($val) {
+            return (filter_var($val, FILTER_VALIDATE_URL) !== false);
+        };
+    }
+
+    public static function email() {
+        return function($val) {
+            return (filter_var($val, FILTER_VALIDATE_EMAIL) !== false);
+        };
+    }
+
+    public static function format($regex) {
+        return function($val) use ($regex) {
+            return preg_match($regex, $value);
+        };
+    }
+
+    public static function confirmation($other_field_func) {
+        return function($val) use ($other_field_func) {
+            return $val === $other_field_func();
+        };
+    }
+
+    public static function inclusion($options) {
+        $list_key = (self::option('in', $options)) ? 'in' : 'within';
+        $list = $options[$list_key];
+        return function($val) use ($list) {
+            return in_array($val, $list, true);
+        };
+    }
+
+    private static function option($name, $options) {
+        return ($options[$name] && $options[$name] === true);
+    }
 
 }
