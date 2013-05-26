15502f2d7454b1aad790d880e8b6a1962928045a
Justin Ridgewell <justin@ridgewell.name>
2013-05-25 21:40:12 -0400

    Refactor Form class

--- lib/FormValidator/Form.php
+++ lib/FormValidator/Form.php
@@ -44,6 +44,38 @@ class Form {
      */
     protected $data = array();
 
+
+
+    ################################################################################
+    ### Pubilc Methods #############################################################
+    ################################################################################
+
+    /**
+     * Returns true if this form has posted
+     * @return Boolean
+     */
+    public function hasPosted() {
+        return isset($_POST[get_class($this)]);
+    }
+
+    /**
+     * Returns true if the form has validation errors
+     * @return Boolean
+     */
+    public function hasErrors() {
+        return (count($this->errors) > 0);
+    }
+
+    /**
+     * //TODO
+     * @param String $name
+     * @return Boolean
+     */
+    public function elementHasError($name) {
+        $error = $this->getDataForName($name, $this->errors);
+        return isset($error);
+    }
+
     /**
      * validates the form against the current validation rules
      * @return array
@@ -53,172 +85,62 @@ class Form {
         array_walk(
             $this->validations,
             array($this, 'validationWalk'),
-            '' // The FieldName will be built recursively
+            '' // The fieldName will be built recursively
         );
         // Call the subclass when the verify is finished, so they don't have to override the validation method
         if (method_exists($this, 'verify')) {
             $this->verify($this->data);
         }
         // Return the data if there isn't any errors
-        return !$this->hasErrors() ? $this->data : null;
-    }
-
-    /**
-     * Validates posted data against a particular rule
-     * @param String $value
-     * @param String $key
-     * @param String $name
-     */
-    protected function validationWalk($value, $key, $fieldName) {
-        if (is_callable($value)) {
-            if (strlen($fieldName) === 0) {
-                $fieldName = $key;
-            }
-            $postedData = $this->getDataForName($fieldName, $_POST);
-            $this->setDataForName($postedData, $fieldName, $this->data);
-            if ($value($postedData) !== true) {
-                $this->invalidateElement($fieldName, 'error');
-            }
-        } else {
-            if ($key === '[]') {
-                $postedKeys = array_keys($this->getDataForName($fieldName, $_POST));
-            } else {
-                $postedKeys = array($key);
-            }
-            foreach($postedKeys as $key) {
-                $fName = $fieldName . '[' . $key . ']';
-                array_walk($value, array($this, 'validationWalk'), $fName);
-            }
-        }
+        return (!$this->hasErrors()) ? $this->data : null;
     }
 
 
-    /**
-     * Returns true if a form has posted
-     * @return Boolean
-     */
-    public function hasPosted() {
-        return $_SERVER['REQUEST_METHOD'] == 'POST';
-    }
 
-    /**
-     * Returns true if the current form has posted, this will only work if you use the $form->submitButtom() function to generate your submit button
-     * @param String $name
-     * @param Int $errorCode
-     * @return Boolean
-     */
-    public function isMe() {
-        return $this->hasPosted() && isset($_POST[get_class($this)]);
-    }
-
-    /**
-     * Returns true if the form has validation errors
-     * @return Boolean
-     */
-    public function hasErrors() {
-        return count($this->errors) > 0;
-    }
-
-    /**
-     * Returns true if the specified form element has an error, also by specifying an error code you can check if the element has a specific error
-     * @param String $name
-     * @param Int $errorCode
-     * @return Boolean
-     */
-    public function elementHasError($name, $errorCode=false) {
-        $error = $this->getDataForName($name, $this->errors);
-        if (isset($error)) {
-            if (!$errorCode) {
-                return true;
-            } else {
-                if (is_array($error)) {
-                    return in_array($errorCode, $error);
-                } else {
-                    return $error == $errorCode;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Invalidates the element $name with the errorcode
-     * @param String $name
-     * @param Int $errorCode
-     */
-    protected function invalidateElement($name, $errorCode) {
-        $element = &$this->getDataForName($name, $this->errors);
-        if (isset($element)) {
-            if (!is_array($element)) {
-                $element = array($element);
-            }
-            $element[] = $errorCode;
-        } else {
-            $this->setDataForName($errorCode, $name, $this->errors);
-        }
-    }
-
-    public function &getDataForName($name, &$base) {
-        $pieces = explode('[', $name);
-        if ($pieces[0] === '') {
-            array_shift($pieces);
-        }
-        foreach ($pieces as $piece) {
-            $piece = preg_replace('/\]$/', '', $piece);
-            if (isset($piece, $base)) {
-                $base = &$base[$piece];
-            } else {
-                $base = NULL;
-                break;
-            }
-        }
-        return $base;
-    }
-
-    protected function setDataForName($data, $name, &$base) {
-        $pieces = explode('[', $name);
-        if ($pieces[0] === '') {
-            array_shift($pieces);
-        }
-        foreach ($pieces as $piece) {
-            $piece = preg_replace('/\]$/', '', $piece);
-            if (isset($piece, $base)) {
-                $base = &$base[$piece];
-            } else {
-                $base[$piece] = array();
-                $base = &$base[$piece];
-            }
-        }
-        $base = $data;
-    }
-
-
-############################################################################################
-# HTML Methods
-############################################################################################
+    ################################################################################
+    ### HTML Methods ###############################################################
+    ################################################################################
 
     /**
      * Echos out any errors the form has
      * @param String $name
-     * @param mixed $message
+     * @param String $message
      */
-    public function error($name, $message) {
-        $error = $this->getDataForName($name, $this->errors);
+    public function error($name, $message = null) {
+        $errors = $this->getDataForName($name, $this->errors);
         if (isset($error)) {
-            if (is_array($message)) {
-                $er = array();
-                foreach ($message as $errorCode => $m) {
-                    if ($this->elementHasError($name, $errorCode)) {
-                        $er[] = '<' . $this->errorWrapperTag . ' class="' . $this->cssErrorClass . '">'
-                            . $m . '</' . $this->errorWrapperTag . '>';
-                    }
-                }
-                echo implode("\n", $er);
+            $output = array();
+            $prettyName = '';
+            if ($message) {
+                $errors = array($message);
             } else {
-                echo '<' . $this->errorWrapperTag . ' class="' . $this->cssErrorClass . '">'
-                    . $message . '</' . $this->errorWrapperTag . '>';
+                preg_match('/(\w+)\]?$', $name, $matches);
+                $prettyName = ucwords($matches[1]);
             }
+            foreach ($errors as $error) {
+                $output[] = sprintf(
+                    '<%s class="%s">%s %s</%s>',
+                    $this->errorWrapperTag,
+                    $this->cssErrorClass,
+                    $prettyName,
+                    $error,
+                    $this->errorWrapperTag
+                );
+            }
+            echo implode("\n", $output);
         }
+    }
+
+    /**
+     * Creates an submit button that this class can identify
+     * @param Mixed $elementAttributes
+     */
+    public function submitButton($value = 'Submit', $elementAttributes=array()) {
+        if (isset($value)) {
+            $elementAttributes['value'] = $value;
+        }
+        $elementAttributes['type'] = 'submit';
+        $this->input(get_class($this), $elementAttributes);
     }
 
     /**
@@ -230,24 +152,23 @@ class Form {
         $defaultAttributes = array(
             'name'  => $name,
             'type'  => 'text',
+            'class' => '',
             'value' => ''
         );
         $attributes = array_merge($defaultAttributes, $elementAttributes);
 
         // Add the error class if the element has an error
         if ($this->elementHasError($name)) {
-            if (isset($attributes['class'])) {
-                $attributes['class'] .= ' ' . $this->cssErrorClass;
-            } else {
-                $attributes['class'] = $this->cssErrorClass;
-            }
+            $attributes['class'] .= ' ' . $this->cssErrorClass;
         }
 
         // Preserve the saved values if the form fails validation
+        // EXCEPT for password fields
+        $value = '';
         if ($attributes['type'] != 'password') {
             $data = $this->getDataForName($name, $this->data);
             if (isset($data)) {
-                $attributes['value'] = $data;
+                $value = htmlentities($data, ENT_QUOTES);
             }
         }
 
@@ -259,69 +180,122 @@ class Form {
 
         // Handle textarea needing a different value format
         if ($attributes['type'] == 'textarea') {
-            echo '<textarea ' . implode(' ', $a) . '>' . $attributes['value'] . '</textarea>';
+            echo '<textarea ' . implode(' ', $a) . '>' . $value . '</textarea>';
         } else {
+            $a[] = sprintf('%s="%s"', 'value', $value);
             echo '<input ' . implode(' ', $a) . ' />';
         }
-    }
-
-    /**
-     * Creates an submit button that this class can identify
-     * @param Mixed $elementAttributes
-     */
-    public function submitButton($value, $elementAttributes=array()) {
-        if (isset($value)) {
-            $elementAttributes['value'] = $value;
-        }
-        $elementAttributes['type'] = 'submit';
-        $this->input(get_class($this), $elementAttributes);
     }
 
     /**
      * Creates a select element
      * @param array $elementAttributes
      * @param array $values
-     * @param Boolean $useKeys
      */
-    public function select($name, $elementAttributes=array(), $values=array(), $useKeys=false) {
+    public function select($name, $elementAttributes=array(), $values=array()) {
         $defaultAttributes = array(
             'name' => $name,
-            'type' => 'normal',
+            'class' => ''
         );
         $attributes = array_merge($defaultAttributes, $elementAttributes);
 
-        $selected   = false;
+        // Add the error class if the element has an error
+        if ($this->elementHasError($name)) {
+            $attributes['class'] .= ' ' . $this->cssErrorClass;
+        }
+
+        $selected = array();
         $data = $this->getDataForName($name, $this->data);
         if (isset($data)) {
-            $selected = $data;
+            $selected = (is_array($data)) ? $data : array($data);
         }
 
         // Convert the name/value key pairs into strings
         $a = array();
         foreach ($attributes as $name => $value) {
-            $a[] = sprintf('%s="%s"', $name, $value);
+            $a[] = sprintf('%s="%s"', $name, htmlentities($value, ENT_QUOTES));
         }
 
         // Echo out the first part of the select element
-        echo '<select ' . implode(' ', $a) . ' >\n';
+        echo '<select ' . implode(' ', $a) . ">\n";
 
         // Echo out the values included within the select element
-        foreach ($values as $value => $name) {
-            $html = '<option ';
-
-            if ($useKeys) {
-                $html .= sprintf('value="%s"', htmlentities($value, ENT_QUOTES));
-                if ($selected == $value || $selected == $name) {
-                    $html .= ' selected="selected" ';
-                }
-            } else {
-                if ($selected == $name) {
-                    $html .= ' selected="selected" ';
-                }
-            }
-            echo $html . '>' . $name . '</option>'  . "\n";
+        foreach ($values as $value => $text) {
+            $selectedText = (in_array($text, $selected)) ? 'selected="selected"' : '';
+            printf("<option %s>%s</option>\n", $selectedText, $name);
         }
         echo '</select>' . "\n";
     }
 
+
+
+    ################################################################################
+    ### Protected Methods ##########################################################
+    ################################################################################
+
+    /**
+     * Validates posted data against a particular rule
+     * @param String $value
+     * @param String $key
+     * @param String $name
+     */
+    protected function validationWalk($validation, $key, $fieldName) {
+        if (is_callable($validation)) {
+            $postedData = $this->getDataForName($key, $_POST);
+            $this->setDataForName($postedData, $key, $this->data);
+            $ret = $validation($postedData);
+            if ($ret !== true) {
+                $this->invalidateElement($key, $ret);
+            }
+        } else {
+            if ($key === '[]') {
+                $postedKeys = array_keys($this->getDataForName($fieldName, $_POST));
+            } else {
+                $postedKeys = array($key);
+            }
+            foreach($postedKeys as $key) {
+                $fName = sprintf('%s[%s]', $fieldName, $key);
+                array_walk($validation, array($this, 'validationWalk'), $fName);
+            }
+        }
+    }
+
+    /**
+     * Invalidates the element $name with the errorcode
+     * @param String $name
+     * @param String $error
+     */
+    protected function invalidateElement($name, $error) {
+        $element = &$this->getDataForName($name, $this->errors);
+        if (isset($element)) {
+            $element[] = $error;
+        } else {
+            $error = array($error);
+            $this->setDataForName($error, $name, $this->errors);
+        }
+    }
+
+    protected function &getDataForName($name, &$base, $create = false) {
+        $pieces = explode('[', $name);
+        if ($pieces[0] === '') {
+            array_shift($pieces);
+        }
+        foreach ($pieces as $piece) {
+            $piece = preg_replace('/\]$/', '', $piece);
+            if (!array_key_exists($piece, $base)) {
+                if (!$create) {
+                    $base = null;
+                    break;
+                }
+                $base[$piece] = array();
+            }
+            $base = &$base[$piece];
+        }
+        return $base;
+    }
+
+    protected function setDataForName($data, $name, &$base) {
+        $base = &$this->getDataForName($name, $base, true);
+        $base = $data;
+    }
 }
