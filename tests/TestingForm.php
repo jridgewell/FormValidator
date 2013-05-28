a03099ff668d7440f63094520323133b44f7ede5
Justin Ridgewell <justin@ridgewell.name>
2013-05-28 01:13:13 -0400

    Rename TestForm to TestingForm

---
+++ tests/TestingForm.php
@@ -0,0 +1,13 @@
+<?php
+
+class TestingForm extends \FormValidator\Form {
+    public function __construct() {
+        $_POST = array(
+            'test' => ''
+        );
+    }
+
+    public function addValidation($key, $value) {
+        $this->setDataForName($key, $value);
+    }
+}
